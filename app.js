e489c60fc813b3c83036297b296bef5705c93acb
Hardik Jain <jainhardik651@gmail.com>
2026-02-25 19:16:30 +0530

    Removed the existing qr on error

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ function generateQR(){
         qr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
     image.classList.add("show-img");
     }else{
-        //qrText = 0;
+        image.classList.remove("show-img");
+        // qr.src = "";
         qrText.classList.add('error');
         setTimeout(() => {
             qrText.classList.remove('error');
