79750a7dbf0bbaa2cbc482cd5c82de91ec31e26c
Hardik Jain <jainhardik651@gmail.com>
2026-02-25 19:09:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//const qrApi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+let image = document.querySelector("#imgBox");
+let qr = document.querySelector("#qrImage");
+let qrText = document.querySelector("#qrText");
+let generate = document.querySelector("#generate");
+
+generate.addEventListener("click", () =>{
+    generateQR();
+});
+
+function generateQR(){
+    if(qrText.value.length > 0){
+        qr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    image.classList.add("show-img");
+    }else{
+        //qrText = 0;
+        qrText.classList.add('error');
+        setTimeout(() => {
+            qrText.classList.remove('error');
+        }, 1000);
+    }
+}
